dd02e594a0cc30003df4a57407672f2496d77492
Brandon <brandonwuedu@gmail.com>
2025-04-12 23:21:42 -0700

    Appends NATO code word for each letter from inputted word into a list

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
 file = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-NATO_alphabet = {row.letter:row.code for (index, row) in file.iterrows()}
-print(NATO_alphabet)
+NATO_alphabet = {row.letter:row.code for (index, row) in file.iterrows()} # takes each letter and its corresponding NATO code and orders it in a dictionary
+# print(NATO_alphabet)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word to translate to NATO spelling? ")
+word = word.upper()
 
+NATO_spelling = [NATO_alphabet[letter] for letter in word] # appends the NATO code word for each letter from inputted word into the list
+print(NATO_spelling)
